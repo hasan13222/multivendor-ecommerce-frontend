6e1d586dc2e957ff499d0318cba6cf5b0d4294e0
hasan13222 <jamil8305@gmail.com>
2025-01-13 23:17:39 +0600

    checkout page updated

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -5,7 +5,15 @@ const Checkout = () => {
     <>
       <div className="products container mx-auto px-3 py-8">
         <h2 className="font-semibold text-3xl mb-5">Checkout</h2>
-        <CheckoutForm />
+        <div className="grid grid-cols-2 items-center">
+                <div className="col-span-2 sm:col-span-1">
+                <CheckoutForm />
+                </div>
+                <div className="col-span-2 sm:col-span-1 max-h-[700px]">
+                 <img className="object-contain" src="/checkout.svg" alt="login" />
+                </div>
+            </div>
+        
       </div>
     </>
   );
